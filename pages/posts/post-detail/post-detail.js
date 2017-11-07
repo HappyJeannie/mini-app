386147ebe13e161b701405d0daec7f140c34a161
jeannie <436030684@qq.com>
2017-11-07 23:37:11 +0800

    music
    add music

--- pages/posts/post-detail/post-detail.js
+++ pages/posts/post-detail/post-detail.js
@@ -5,6 +5,7 @@ Page({
    * 页面的初始数据
    */
   data: {
+    isPlayMusic:false
   },
 
   /**
@@ -176,10 +177,22 @@ Page({
     })
   },
   onMusicTap:function(event){
-    //音乐播放处理
-    wx.playBackgroundAudio({
-      dataUrl: 'http://dl.stream.qqmusic.qq.com/C400001udJ173V0rrp.m4a?vkey=B57E316A818B4DB6109F5194D429D53ABA44B1CBD87C290510B7444C36A27B04AB4B30CFCB617747257C6615FFB1952287A1D10EAC68EA12&guid=5710102420&uin=0&fromtag=66',
-      title:"遗憾",     coverImgUrl:"https://y.gtimg.cn/music/photo_new/T002R300x300M000004VO2Bs2IQqN5.jpg?max_age=2592000"
-    })
+    var isPlayMusic = this.data.isPlayMusic;
+    if (isPlayMusic){
+      wx.pauseBackgroundAudio();
+      this.setData({
+        isPlayMusic:false
+      });
+    }else{
+      //音乐播放处理
+      wx.playBackgroundAudio({
+        dataUrl: 'http://dl.stream.qqmusic.qq.com/C400001udJ173V0rrp.m4a?vkey=B57E316A818B4DB6109F5194D429D53ABA44B1CBD87C290510B7444C36A27B04AB4B30CFCB617747257C6615FFB1952287A1D10EAC68EA12&guid=5710102420&uin=0&fromtag=66',
+        title: "遗憾", coverImgUrl: "https://y.gtimg.cn/music/photo_new/T002R300x300M000004VO2Bs2IQqN5.jpg?max_age=2592000"
+      })
+      this.setData({
+        isPlayMusic: true
+      });
+    }
+    
   }
 })
