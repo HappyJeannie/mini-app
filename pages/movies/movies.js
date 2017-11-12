e85f632dc6a13b18da2e2c7b054062ab77977eec
jeannie <436030684@qq.com>
2017-11-12 23:50:14 +0800

    Douban Data
    Add Douban Data in movie pages

--- pages/movies/movies.js
+++ pages/movies/movies.js
@@ -1,3 +1,4 @@
+var util = require("../../utils/util.js");
 // pages/movies/movies.js
 Page({
 
@@ -5,62 +6,115 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    inTheaters:{},
+    comingSoon:{},
+    top250:{}
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    var inTheatersUrl = "/v2/movie/in_theaters"+"?start=0&&count=3";
+    var comingSoonUrl = "/v2/movie/coming_soon" + "?start=0&&count=3";
+    var top250Url = "/v2/movie/top250" + "?start=0&&count=3";
+    this.getMovieListData(inTheatersUrl,"inTheaters");
+    this.getMovieListData(comingSoonUrl,"comingSoon");
+    this.getMovieListData(top250Url,"top250");
   },
+  getMovieListData: function (url,settedKey) {
+    var that = this;
+    //获取数据
+    wx.request({
+      url: util.api + url,
+      method: "GET",
+      header: {
+        "content-Type": "application/xml"
+      },
+      success: function (res) {
+        console.log(res.data);
+        that.processDoubanData(res.data, settedKey);
+      },
+      fail: function (res) {
+        console.log(res);
+      },
+      complete: function () {
 
+      }
+    });
+  },
+  processDoubanData: function (moviesDouban, settedKey){
+    //处理获取到的数据
+    var movies = [];
+    for (var idx in moviesDouban.subjects){
+      var subject = moviesDouban.subjects[idx];
+      var title = subject.title;
+      if(title.lenght >= 6){
+        title = title.substring(0,6)+"...";
+      }
+      //console.log(subject.rating.average);
+      //util.convertToStrigArray(subject.rating.average);
+      var temp = {
+        title :title,
+        average : subject.rating.average,
+        coverageUrl : subject.images.large,
+        movieId : subject.id
+      }
+      movies.push(temp);
+    }
+    // 以下代码为javascript动态代码编写格式
+    var readyData = {};
+    readyData[settedKey] = {
+      movies: movies
+    };
+    this.setData(readyData);
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   }
 })
--- utils/util.js
+++ utils/util.js
@@ -13,7 +13,23 @@ const formatNumber = n => {
   n = n.toString()
   return n[1] ? n : '0' + n
 }
+const api = "https://api.douban.com";
+
+function convertToStrigArray(stars){
+  var num = stars.toString().tosubstrig(0,1);
+  console.log(num);
+  var array = [];
+  for(var i = 1; i < 5; i ++){
+    if(i <= num){
+      array.push(1);
+    }else{
+      array.push(0);
+    }
+  }
+};
 
 module.exports = {
-  formatTime: formatTime
+  formatTime: formatTime,
+  api : api,
+  convertToStrigArray: convertToStrigArray
 }
